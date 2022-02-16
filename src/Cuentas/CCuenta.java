5cb8f69d7ba0a87c50da32643538504d4fda5cfa
zelenita12 <zel_elena@hotmail.com>
2022-02-16 20:59:36 +0100

    El resultado de la ejecución ha sido correcto.

--- src/Cuentas/CCuenta.java
+++ src/Cuentas/CCuenta.java
@@ -1,4 +1,11 @@
 package Cuentas;
+/**
+     * @author Elena Garcia
+     * 
+     * @version v 0.1 Febrero 2022
+     * 
+     * Se crea una clase para representar una cuenta bancaria
+     */
 
 public class CCuenta {
 
@@ -57,33 +64,50 @@ public class CCuenta {
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
-
+     /** 
+     * @param nombre Es tipo String y da el nombre del propietario de la cuenta.
+     * @param cuenta Es tipo Strind y da el nombre de la cuenta.
+     * @param saldo El tipo souble y recoge el saldo de la cuenta
+     * @param tipoInteres Es tipo double y recoge el tipo de interés.
+     */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-    
+    /**
+    * El constructor de la clase cuenta. 
+    */
     public CCuenta() {
     }
-
+    /**
+    * Este constructor se encarga de recoger los diferentes parametros. 
+    */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
-
+    /**
+     * @return the saldo
+     */
     public double estado() {
         return getSaldo();
     }
-
+    /**
+    * Este método se encarga de ingresar la cantidad de dinero 
+    * Lanza una excepción
+    */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
         }
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+    * Este método se encarga de iingresar la cantidad de dinero 
+    * Lanza una excepción
+    */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0) {
             throw new Exception("No se puede retirar una cantidad negativa");
